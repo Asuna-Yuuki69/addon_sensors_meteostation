fb1784957840b510c3594905fff06170c7bcd676
Asuna-Yuuki69 <dzejdlik2016@gmail.com>
2025-01-14 08:20:29 +0100

    delet unnecessary wifi and mqtt over wifi script <3

--- src/main.cpp
+++ src/main.cpp
@@ -51,22 +51,35 @@ char buffer[1024] = {0};
 char username[] = "meteo";
 char password[] = "<Meteo123..";
 char clientID[] = "meteoespdomecek";
+int data_channel = 0;
+
+
+bool isConnect()
+{
+    modem.sendAT("+SMSTATE?");
+    if (modem.waitResponse("+SMSTATE: ")) {
+      String res = modem.stream.readStringUntil('\r');
+      return res.toInt();
+
+    }
+    return false;
+}
 #define DHTPIN 10     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT22
 #define ANEMOMETER_PIN 13 // The pin connected to the anemometer signal
-
 volatile unsigned int pulseCount = 0; // Counter for pulses
 float windSpeed = 0.0; // Calculated wind speed (m/s or other units depending on calibration)
 unsigned long lastMillis = 0; // Track the last time we calculated wind speed
 const float CALIBRATION_FACTOR = 0.5; // Adjust based on your anemometer (e.g., pulses per m/s)
-
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
-const char* ssid = "iot";
-const char* password = "fe801240f3fffe7c";
-const char* mqttServer = "xtomi.czella.net";
-const int mqttPort = 64525;
-const char* mqttUser = "meteo";
-const char* mqttPassword = "Meteo123.."; // No password
+
+
+//const char* ssid = "iot";
+//const char* password = "fe801240f3fffe7c";
+//const char* mqttServer = "xtomi.czella.net";
+//const int mqttPort = 64525;
+//const char* mqttUser = "meteo";
+//const char* mqttPassword = "Meteo123.."; // No password
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -81,7 +94,7 @@ struct WindDirection {
   float resistance;
   float degrees;
 };
-
+ //vane caculation constants
 WindDirection windDirections[] = {
   {33000, 0},
   {6570, 22.5},
@@ -120,32 +133,7 @@ void countPulse() {
   pulseCount++; // Increment pulse counter for each interrupt
 }
 
-void connectToWiFi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("\nwifiok");
-  Serial.print("IP:");
-  Serial.println(WiFi.localIP());
-}
-
-void connectToMQTT() {
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP32Client", mqttUser, mqttPassword)) {
-      Serial.println("Connected to MQTT");
-    } else {
-      Serial.print("Failed MQTT connection, rc=");
-      Serial.print(client.state());
-      Serial.println(" trying again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
 
 const int sensorPin = 03; // ADC pin for wind vane sensor
 
@@ -160,11 +148,7 @@ void setup()
   pinMode(ANEMOMETER_PIN, INPUT_PULLUP); // Set the pin as input with pullup resistor
   attachInterrupt(digitalPinToInterrupt(ANEMOMETER_PIN), countPulse, FALLING); // Interrupt on falling edge
 
-  connectToWiFi();
-  delay(10000);
 
-  client.setServer(mqttServer, mqttPort);
-  connectToMQTT();
 
   analogReadResolution(12); // Set ADC resolution to 12 bits
 }
@@ -229,31 +213,7 @@ void loop()
     Serial.print("Humidity =");
     Serial.println(hum);
 
-    // Publish to MQTT every 30 seconds
-    if (currentMillis - lastMqttPublishMillis >= 30000) {
-      if (!client.connected()) {
-        connectToMQTT();
-      }
 
-      client.loop(); // Maintain MQTT connection
-
-      char tempStr[8];
-      char humStr[8];
-      char windStr[8];
-
-      dtostrf(temp, 6, 2, tempStr); // Convert temperature to string
-      dtostrf(hum, 6, 2, humStr);   // Convert humidity to string
-      dtostrf(windSpeed, 6, 2, windStr); // Convert wind speed to string
-
-      client.publish("meteo/temperature", tempStr);
-      client.publish("meteo/humidity", humStr);
-      client.publish("meteo/windSpeed", windStr);
-      client.publish("meteo/windDirection", String(windDirection).c_str());
-
-      Serial.println("MQTT Data Published");
-
-      lastMqttPublishMillis = currentMillis;
-    }
 
     // 2000ms delay between reads
     delay(2000);
